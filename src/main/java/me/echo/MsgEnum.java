fd4808360e05b4d57ea536b197038b6e11249ca8
MyHusky <MyHasky@hotmail.com>
2020-07-21 11:03:03 +0800

    使用json返回数据

---
+++ src/main/java/me/echo/MsgEnum.java
@@ -0,0 +1,29 @@
+package me.echo;
+
+public enum MsgEnum {
+    OK(100, "success"), FAILED(101, "failed");
+
+    private Integer code;
+    private String msg;
+
+    MsgEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ src/main/java/me/echo/bean/Msg.java
@@ -0,0 +1,60 @@
+package me.echo.bean;
+
+import me.echo.MsgEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 通用返回客户端的类
+ */
+public class Msg {
+    private int code;
+
+    private String msg;
+
+    private Map<String, Object> extend = new HashMap<>();
+
+    public static Msg success(){
+        Msg msg = new Msg();
+        msg.setCode(MsgEnum.OK.getCode());
+        msg.setMsg(MsgEnum.OK.getMsg());
+        return msg;
+    }
+
+    public static Msg fail(){
+        Msg msg = new Msg();
+        msg.setCode(MsgEnum.FAILED.getCode());
+        msg.setMsg(MsgEnum.FAILED.getMsg());
+        return msg;
+    }
+
+    public Msg add(String key, Object value) {
+        this.getExtend().put(key, value);
+        return this;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Map<String, Object> getExtend() {
+        return extend;
+    }
+
+    public void setExtend(Map<String, Object> extend) {
+        this.extend = extend;
+    }
+}
